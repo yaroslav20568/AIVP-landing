e0d6a22f7aa7d46d611a763fb9b143087b6e871e
oleg_nbd <oleg@nbd.by>
2022-06-08 18:06:52 +0300

    modals and modals js

--- src/js/app.js
+++ src/js/app.js
@@ -13,4 +13,61 @@ window.addEventListener('DOMContentLoaded', () => {
 			burger.classList.remove('burger--active');
 		}
 	});
+
+	const contactModal = document.querySelector('.contact-modal');
+	const partnerModal = document.querySelector('.partner-modal');
+
+	const headerBtn = document.querySelector('.header__btn');
+	const cooperationBtn = document.querySelector('.cooperation__btn');
+
+
+	const openModal = (modal) => {
+		modal.classList.add('modal--active')
+
+		outsideClickModal(modal);
+
+		escEnterModal(modal);
+	};
+
+	const closeModal = (modal) => {
+		modal.classList.remove('modal--active')
+	};
+
+	const contactBtns = document.querySelectorAll('.btn--contact');
+	const partnerBtns = document.querySelectorAll('.btn--partner');
+
+	const outsideClickModal = (modal) => {
+		modal.firstElementChild.addEventListener('click', () => {
+			closeModal(modal);
+		});
+	};
+
+	// const escEnterModal = (modal) => {
+		// document.addEventListener('keyup', function(e){
+		// 	if(e.keyCode === 27) {
+		// 		closeModal(modal);
+		// 	}
+		// });
+	// };
+
+	contactBtns.forEach(contactBtn => {
+		contactBtn.addEventListener('click', () => {
+			openModal(contactModal);
+		})
+	});
+
+	partnerBtns.forEach(partnerBtn => {
+		partnerBtn.addEventListener('click', () => {
+			openModal(partnerModal);
+		})
+	});
+
+	
+	const modalCloseBtns = document.querySelectorAll('.modal__close');
+
+	modalCloseBtns.forEach(modalCloseBtn => {
+		modalCloseBtn.addEventListener('click', () => {
+			modalCloseBtn.parentElement.parentElement.parentElement.classList.remove('modal--active');
+		});
+	})
 });
